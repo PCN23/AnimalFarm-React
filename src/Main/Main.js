425e5f3e594f5e5ee0d65f427f0d967077ab8175
Pedro Chavez Nolasco <pedro.chn01@gmail.com>
2022-06-07 12:51:26 -0700

    starting

---
+++ src/Main/Main.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '/Main.css';
+import backgroundImg from '../background.png';
+
+export default function Main({ animals }) {
+    return (
+        <main style={{ backgroundImage: `url(${backgoundImg})` }}>
+
+        {animals.map((animals, i) => <)}
+
+        </main>
+    )
+
+
+}
